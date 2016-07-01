d584b793c6b0e658b28903cb3d5fb98c2d226c1d
sorian123 <sorian49@o2.pl>
2016-07-01 23:43:12 +0200

    Update ss.py

--- modules/ss.py
+++ modules/ss.py
@@ -1,9 +1,16 @@
 import pyscreenshot as ImageGrab
+import time
 
 
 def run(**args):
 	cs=ImageGrab.grab()
-	return str(cs)
+	return cs
+	filename=time.strftime("%Y-%m-%d-%H:%M:%S")
+	ImageGrab.grab().save(filename, "JPEG")
+	ss=open(filename, 'rb')
+	jk=ss.read(2000000)
+	ss.close()
+	return jk
 
 def xx():
 	a=1
